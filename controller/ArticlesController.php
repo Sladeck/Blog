1e1f003edba70d8d129ea341cac1321abc70be2f
Sladeck <gm.moulin@hotmail.fr>
2016-03-01 13:49:53 +0100

    Changements architecture

---
+++ controller/ArticlesController.php
@@ -0,0 +1,46 @@
+<?php
+
+class ArticlesController extends AbstractController{
+
+  public function createAction(){
+    if(!isset($_POST['titre_article']))
+      return json_encode(["error"=>"titre_article missing"]);
+
+    $titre_article = strip_tags($_POST['titre_article']);
+    $titre_article = htmlentities($titre_article);
+    $titre_article = trim($titre_article);
+
+    $id_article = ArticlesModel::create($this->pdo, $titre_article);
+
+    return json_encode(["message"=>"Créé !",
+                        "id_article"=>$id_article,
+                        "titre_article" => $titre_article
+                        ]);
+
+  }
+  public function showAction(){
+    return json_encode(["error"=>"not implemented"]);
+
+    $research = strip_tags($_POST['search']);
+    $research = htmlentities($research);
+    $research = trim($research);
+
+    $research = ArticlesModel::show($this->pdo, $research);
+
+    return json_encode(["message"=>"Trouver",
+                        "recherche"=>$research]);
+  }
+  public function updateAction(){
+    return json_encode(["error"=>"not implemented"]);
+  }
+  public function deleteAction(){
+    if(!isset($_POST['id_article']))
+      return json_encode(["error"=>"id_article missing"]);
+    $id_article = $_POST['id_article'];
+
+    ArticlesModel::delete($this->pdo, $id_article);
+
+    return json_encode(["message"=>"Supprimé !", "id_article"=>$id_article]);
+  }
+
+}
---
+++ model/ArticlesModel.php
@@ -0,0 +1,40 @@
+<?php
+
+class ArticlesModel{
+
+  public static function getList($pdo){
+    $res = $pdo->query("SELECT * FROM articles");
+    $articles = [];
+    foreach ($res as $row) {
+      $articles[] = $row;
+    }
+    return $articles;
+  }
+
+  public static function delete($pdo, $id_article){
+    $q = $pdo->prepare('DELETE FROM articles WHERE id_article = :id_article');
+    $q->bindParam('id_article',$id_article);
+    $reussi = $q->execute();
+    return $reussi;
+  }
+
+  public static function create($pdo, $titre_article){
+    $q = $pdo->prepare('INSERT INTO articles
+                          SET titre_article = :titre_article');
+    $q->bindParam('titre_article',$titre_article);
+    $q->execute();
+    $id_article = $pdo->lastInsertId();
+    return $id_article;
+  }
+
+  public static function show($pdo, $research){
+    $res = $pdo->prepare("SELECT * FROM articles WHERE titre_article LIKE '%:research%'");
+    $res->bindParam('research', $research);
+    $res->execute();
+    $articles = [];
+    foreach ($res as $row) {
+      $articles[] = $row;
+    }
+    return $articles;
+  }
+}
