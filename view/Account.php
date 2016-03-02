a116afdb2007cc855cfd03932cb7b73681cbb6e6
clemferreol <cleol@9online.fr>
2016-03-02 17:14:09 +0100

    Account

---
+++ view/Account.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="iso-8859-1">
+  <title>Connexion</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</head>
+<body>
+ 
+
+  <form class="my-form">
+    <input type="text" name="name" placeholder="Nom"><br><br>
+    <input type="text" name="firstname" placeholder="Prénom"><br><br>
+    <input type="text" name="pseudo" placeholder="Pseudo"><br><br>
+    <input type="text" name="mdp" placeholder="Mot De Passe"><br><br>
+    <input type="text" name="email" placeholder="Email"><br><br>
+    
+    <input type="submit" value="Modifier">
+    </form>
+
+</body>
+</html>
